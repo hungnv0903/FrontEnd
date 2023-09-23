7d0edd960e49b9cb6b9b07ed4125a1fb5b4d28c8
unknown <nguyenhung090399@gmail.com>
2023-09-23 08:51:19 +0700

    FrontEnd FPT Academy

---
+++ BasicJS/main.js
@@ -0,0 +1,19 @@
+var a = [1,4,3,5,6,10,9] ; 
+var max = a[0] ; 
+var index
+for(i = 1 ; i<a.length ; i++){
+    if(a[i]>max){
+        max = a[i] ;
+        index = i ;  
+    }
+}
+console.log(max) ;
+console.log("index : " , index) 
+
+var gt = 1 ; 
+gt = "nguyenvanhung" 
+console.log(gt) ; 
+
+
+var str = "a"+10 ; 
+console.log(typeof(str)) ; 
